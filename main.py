426a3a8a398ca50836543074df7aba3442de4053
nguoimoi <buinguyenminhquan84@gmail.com>
2025-11-16 16:12:00 +0700

    no

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ app.register_blueprint(github_bp, url_prefix="/login")
 app.register_blueprint(auth_bp, url_prefix="/auth")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+#     app.run(host="0.0.0.0", port=3000, debug=True)
 
-logging.getLogger('pymongo').setLevel(logging.ERROR)
-logging.getLogger("urllib3").setLevel(logging.WARNING)
-logging.getLogger('flask').setLevel(logging.WARNING)
-logging.getLogger().setLevel(logging.WARNING)
+# logging.getLogger('pymongo').setLevel(logging.ERROR)
+# logging.getLogger("urllib3").setLevel(logging.WARNING)
+# logging.getLogger('flask').setLevel(logging.WARNING)
+# logging.getLogger().setLevel(logging.WARNING)
+    pass
